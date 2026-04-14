7186e73f51574ac6d6c11010250accc110b1bb9c
Yashvir Gaming <64865096+YashvirGaming@users.noreply.github.com>
2026-04-14 12:15:27 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,14 @@
+import os
 import sys
+
 from PySide6.QtWidgets import QApplication
-from PySide6.QtGui import QFontDatabase
 
 from gui.main_window import MainWindow
-
-
-def load_fonts():
-    QFontDatabase.addApplicationFont("assets/fonts/Orbitron-Regular.ttf")
-    QFontDatabase.addApplicationFont("assets/fonts/Orbitron-Bold.ttf")
 
 
 def main():
     app = QApplication(sys.argv)
     app.setApplicationName("HTTP Debugger Lite - Yashvir Gaming")
-
-    load_fonts()
 
     window = MainWindow()
     window.show()
