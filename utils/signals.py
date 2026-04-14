7e41a1a88ffd9207f7ac626238a988b1388cd558
Yashvir Gaming <64865096+YashvirGaming@users.noreply.github.com>
2026-04-14 12:21:31 +0400

    Delete utils directory

--- utils/signals.py
+++
@@ -1,8 +0,0 @@
-from PySide6.QtCore import QObject, Signal
-
-
-class AppSignals(QObject):
-    request_selected = Signal(int)
-    start_capture = Signal()
-    pause_capture = Signal()
-    clear_requests = Signal()
